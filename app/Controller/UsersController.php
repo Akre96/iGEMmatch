f9a669fb9f28f7c4a38d7b4d2f24c3d919cb92dd
Samir Akre <samir.akre@gmail.com>
2016-05-24 16:20:12 -0700

    front

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -120,15 +120,8 @@ class UsersController extends AppController {
 	public function logout() {
     return $this->redirect($this->Auth->logout());
 	}	
-	
-	public function login() {
-    if ($this->request->is('post')) {
-        if ($this->Auth->login()) {
-            return $this->redirect($this->Auth->redirectUrl());
-        }
-        $this->Flash->error(__('Invalid username or password, try again'));
-    }
-}
+
+
 }
 
 
