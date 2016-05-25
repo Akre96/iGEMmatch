57a4f4d0950659f63ba9f1d86be618439b30fdd1
Samir Akre <samir.akre@gmail.com>
2016-05-24 17:25:08 -0700

    front

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -41,8 +41,13 @@ class AppController extends Controller {
             'logoutRedirect' => array(
                 'controller' => 'categories',
                 'action' => 'index',
-            ))
-
+            ),
+            'authenticate' => array(
+                'Form' => array(
+                    'passwordHasher' => 'Blowfish'
+                )
+            )
+        )
     );
 
     public function beforeFilter() {
--- app/Model/User.php
+++ app/Model/User.php
@@ -1,6 +1,6 @@
 <?php
 App::uses('AppModel', 'Model');
-
+App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
 /**
  * User Model
  *
@@ -10,7 +10,13 @@ App::uses('AppModel', 'Model');
 class User extends AppModel {
 
 public function beforeSave($options = array()) {
-
+    if (isset($this->data[$this->alias]['password'])) {
+        $passwordHasher = new BlowfishPasswordHasher();
+        $this->data[$this->alias]['password'] = $passwordHasher->hash(
+            $this->data[$this->alias]['password']
+        );
+    }
+    return true;
 }
 
 /**
