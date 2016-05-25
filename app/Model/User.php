d52c861c5f7aafdbdbd4e603ec68b74f1f121390
Samir Akre <samir.akre@gmail.com>
2016-05-24 17:20:04 -0700

    front

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,6 +1,6 @@
 <?php
 App::uses('AppModel', 'Model');
-App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
+App::uses('Controller/Component/Auth');
 /**
  * User Model
  *
@@ -10,13 +10,7 @@ App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
 class User extends AppModel {
 
 public function beforeSave($options = array()) {
-    if (isset($this->data[$this->alias]['password'])) {
-        $passwordHasher = new BlowfishPasswordHasher();
-        $this->data[$this->alias]['password'] = $passwordHasher->hash(
-            $this->data[$this->alias]['password']
-        );
-    }
-    return true;
+
 }
 
 /**
