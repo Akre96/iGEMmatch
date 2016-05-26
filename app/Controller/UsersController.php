6ff73914e6a9a804ee607b90fe9965f0442e1307
Samir Akre <samir.akre@gmail.com>
2016-05-25 22:39:47 -0700

    front

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -122,6 +122,14 @@ class UsersController extends AppController {
          
         // if we get the post information, try to authenticate
         if ($this->request->is('post')) {
+        	$this_user = $this->User->find('first', array(
+            'conditions' => array('username' => $this->request->data['User']['username'])
+        	));
+            $user_id = $this_user['User']['id'];
+        	$this->request->data['User'] = array_merge(
+                $this->request->data['User'],
+                array('id' => $user_id)
+        		);
             if ($this->Auth->login($this->request->data)) {
                 $this->Session->setFlash(__('Welcome, '. $this->Auth->user('username')));
                 $this->redirect($this->Auth->redirectUrl());
