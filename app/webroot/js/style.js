32b529fd9af2f7a15ab93623ea89ad7d7ab63b54
Samir Akre <samir.akre@gmail.com>
2016-06-04 23:07:00 -0700

    front

--- app/webroot/js/style.js
+++ app/webroot/js/style.js
@@ -3,15 +3,7 @@ $(document).ready(function(){
 
 
 $("#title").hover(function(){
-	$(this).animate({
-		border: "1px solid #FF0D02",
-		color: "#FF0D02"
-		}, 5000,function(){ });
-	},function(){
-		$(this).animate({
-		border: "none",
-		color: "#FF0D02"
-		}, 5000,function(){ });
+	alert('swag');
 	});
 
 
