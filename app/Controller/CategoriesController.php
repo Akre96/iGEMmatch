96f4033e9fcd0b8348c5ce06b8ee8b04bc76eda2
Samir Akre <samir.akre@gmail.com>
2016-06-04 23:32:29 -0700

    front

--- app/Controller/CategoriesController.php
+++ app/Controller/CategoriesController.php
@@ -51,17 +51,7 @@ class CategoriesController extends AppController {
  *
  * @return void
  */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->Category->create();
-			if ($this->Category->save($this->request->data)) {
-				$this->Flash->success(__('The category has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->error(__('The category could not be saved. Please, try again.'));
-			}
-		}
-	}
+
 
 /**
  * edit method
@@ -70,22 +60,7 @@ class CategoriesController extends AppController {
  * @param string $id
  * @return void
  */
-	public function edit($id = null) {
-		if (!$this->Category->exists($id)) {
-			throw new NotFoundException(__('Invalid category'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->Category->save($this->request->data)) {
-				$this->Flash->success(__('The category has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->error(__('The category could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('Category.' . $this->Category->primaryKey => $id));
-			$this->request->data = $this->Category->find('first', $options);
-		}
-	}
+
 
 /**
  * delete method
@@ -94,17 +69,5 @@ class CategoriesController extends AppController {
  * @param string $id
  * @return void
  */
-	public function delete($id = null) {
-		$this->Category->id = $id;
-		if (!$this->Category->exists()) {
-			throw new NotFoundException(__('Invalid category'));
-		}
-		$this->request->allowMethod('post', 'delete');
-		if ($this->Category->delete()) {
-			$this->Flash->success(__('The category has been deleted.'));
-		} else {
-			$this->Flash->error(__('The category could not be deleted. Please, try again.'));
-		}
-		return $this->redirect(array('action' => 'index'));
-	}
+
 }
