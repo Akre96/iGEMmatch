4d7a2993f9924be258a5819a2f7885ff5f9c1110
Samir Akre <samir.akre@gmail.com>
2016-06-09 04:08:41 -0700

    front

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -93,7 +93,7 @@ public function beforeFilter() {
 			$this->User->create();
 			if ($this->User->save($this->request->data)) {
 				$this->Flash->success(__('The user has been saved.'));
-				return $this->redirect(array('action' => 'index'));
+				return $this->redirect(array('action' => 'edit'));
 			} else {
 				$this->Flash->error(__('The user could not be saved. Please, try again.'));
 			}
