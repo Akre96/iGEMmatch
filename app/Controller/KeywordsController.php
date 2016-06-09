0a50f16593a5b12ab67f7500090b481b85cdc8cd
Samir Akre <samir.akre@gmail.com>
2016-06-09 04:22:02 -0700

    front

--- app/Controller/KeywordsController.php
+++ app/Controller/KeywordsController.php
@@ -73,7 +73,7 @@ class KeywordsController extends AppController {
 		if ($this->request->is(array('post', 'put'))) {
 			if ($this->Keyword->save($this->request->data)) {
 				$this->Flash->success(__('The keyword has been saved.'));
-				return $this->redirect(array('action' => 'index'));
+				return $this->redirect(array('controller'=>'users','action' => 'edit'));
 			} else {
 				$this->Flash->error(__('The keyword could not be saved. Please, try again.'));
 			}
