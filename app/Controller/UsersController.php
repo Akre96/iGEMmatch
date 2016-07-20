2f3790733817c3845b86799a93fc13027101ac1e
Samir Akre <samir.akre@gmail.com>
2016-07-20 16:08:37 -0700

    pagination limit increase

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -31,7 +31,7 @@ public function beforeFilter() {
  */
 	public function index() {
 	//	$this->User->recursive = 0;
-		$this->set('users', $this->Paginator->paginate());
+		$this->set('users', $this->Paginator->paginate($paginate));
 	}
 
 /**
